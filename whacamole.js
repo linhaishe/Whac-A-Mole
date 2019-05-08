e126ee05764ceb0c84788bc55c9c0f2aa61b2e60
xusumu <xusumu@gmail.com>
2019-05-08 17:52:55 +0800

    添加注释

--- whacamole.js
+++ whacamole.js
@@ -1,13 +1,14 @@
-var td = new Array();
-var playing = false;
-var score = 0;
-var beat = 0;
-var success = 0;
-var knock = 0;
-var countDown = 30;
-var interId = null,
-    timeId = null;
+var td = new Array(); //保存每个格子的地鼠
+var playing = false; //游戏是否开始
+var score = 0; //分数
+var beat = 0; //鼠标点击次数
+var success = 0; //命中率
+var knock = 0; //鼠标点中老鼠图片次数
+var countDown = 30; //倒计时
+var interId = null, // 指定setInterval()的变量
+    timeId = null; //指定setTimeout()的变量
 
+// 游戏结束
 function GameOver(){
     timeStop();
     playing = false;
@@ -19,7 +20,8 @@ function GameOver(){
     beat = 0;
     countDown = 30;
 }
-    
+
+//显示当前倒计时所剩时间
 function timeShow(){
     document.form1.remtime.value = countDown;
     if(countDown == 0)
@@ -34,11 +36,13 @@ function timeShow(){
         }
 }
 
+//主动停止所有时间
 function timeStop(){
     clearInterval(interId);//clearInterval()方法返回setInterval()方法的id
     clearTimeout(timeId);//clearTime()方法返回setTimeout()的id
 }
-    
+
+//随机循环显示老鼠图片
 function show(){
     if(playing)
     {
@@ -48,6 +52,7 @@ function show(){
     }
 }
 
+//清除所有老鼠图片
 function clearMouse(){
     for(var i=0;i<=24;i++)
     {
@@ -55,6 +60,7 @@ function clearMouse(){
     }
 }
 
+//点击事件函数，判断是否点中老鼠
 function hit(id){
     if(playing==false)
     {
@@ -83,6 +89,7 @@ function hit(id){
     }
 }
 
+//游戏开始
 function GameStart(){
     playing = true;
     interId = setInterval("show()",1000);
