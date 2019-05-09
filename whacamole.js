d3e224e1723de8677cecb90c8d550cc55f2998f2
xusumu <xusumu@gmail.com>
2019-05-09 16:16:08 +0800

    添加问题注释

--- whacamole.js
+++ whacamole.js
@@ -1,4 +1,4 @@
-var td = new Array(); //保存每个格子的地鼠
+var td = new Array(); //保存每个格子的地鼠？？？？？？？？？？？
 var playing = false; //游戏是否开始
 var score = 0; //分数
 var beat = 0; //鼠标点击次数
@@ -8,57 +8,41 @@ var countDown = 30; //倒计时
 var interId = null, // 指定setInterval()的变量
     timeId = null; //指定setTimeout()的变量，NULL是什么变量？？？？？？
 
-// 游戏结束
-function GameOver(){
-    timeStop();
-    playing = false;
-    clearMouse();
-    alert("游戏结束！\n你获得的分数为："+score+"\n命中率为："+success);
-    success = 0;
-    score = 0;
-    knock = 0;
-    beat = 0;
-    countDown = 30;
+//游戏开始
+function GameStart(){
+    playing = true;
+    interId = setInterval("show()",1000);//每一秒循环显示一次老鼠图片
+    document.form1.score.value = score;//指定方式需要熟悉，如何用JS更改文档流信息？Input Text value 属性√
+    document.form1.success.value = success;
+    timeShow();
+}
+
+//随机循环显示老鼠图片
+function show(){
+    if(playing)//if的语法是什么？？？？？？
+    {
+        var current = Math.floor(Math.random()*25);
+        //设置格子随机，图片随机
+        document.getElementById("td["+current+"]").innerHTML = '<img src="img/mouse.png">';
+        //每三秒出现一次什么东西？？？？？？？？？？？？？？？？？
+        setTimeout("document.getElementById('td["+current+"]').innerHTML=''",3000);
+        //为什么不是getElementById("td[current]")???????
+    }
 }
 
 //显示当前倒计时所剩时间
 function timeShow(){
-    //指定方式需要熟悉！！！！！！？？？？
     document.form1.remtime.value = countDown;
     if(countDown == 0)
         {
             GameOver();
-            return;//return个啥？？？？？？
+            return;//return 语句会终止函数的执行并返回函数的值
         }
     else
         {
             countDown = countDown-1;
             timeId = setTimeout("timeShow()",1000);
         }
-}
-
-//主动停止所有时间
-function timeStop(){
-    clearInterval(interId);//clearInterval()方法返回setInterval()方法的id
-    clearTimeout(timeId);//clearTime()方法返回setTimeout()的id
-}
-
-//随机循环显示老鼠图片
-function show(){
-    if(playing)
-    {
-        var current = Math.floor(Math.random()*25);
-        document.getElementById("td["+current+"]").innerHTML = '<img src="img/mouse.png">';
-        setTimeout("document.getElementById('td["+current+"]').innerHTML=''",3000);
-    }
-}
-
-//清除所有老鼠图片
-function clearMouse(){
-    for(var i=0;i<=24;i++)
-    {
-        document.getElementById("td["+i+"]").innerHTML="";
-    }
 }
 
 //点击事件函数，判断是否点中老鼠
@@ -70,11 +54,11 @@ function hit(id){
     }
     else
     {	
-         beat +=1;
+         beat += 1;
         if(document.getElementById("td["+id+"]").innerHTML!="")
         {
             score += 1;
-            knock +=1;
+            knock += 1;
             success = knock/beat;
             document.form1.success.value = success;
             document.form1.score.value = score;
@@ -90,11 +74,29 @@ function hit(id){
     }
 }
 
-//游戏开始
-function GameStart(){
-    playing = true;
-    interId = setInterval("show()",1000);
-    document.form1.score.value = score;
-    document.form1.success.value = success;
-    timeShow();
+//主动停止所有时间
+function timeStop(){
+    clearInterval(interId);//clearInterval()方法返回setInterval()方法的id
+    clearTimeout(timeId);//clearTime()方法返回setTimeout()的id
+}
+
+//清除所有老鼠图片
+function clearMouse(){
+    for(var i=0;i<=24;i++)
+    {
+        document.getElementById("td["+i+"]").innerHTML="";
+    }
+}
+
+// 游戏结束
+function GameOver(){
+    timeStop();
+    playing = false;
+    clearMouse();
+    alert("游戏结束！\n你获得的分数为："+score+"\n命中率为："+success);
+    score = 0;
+    beat = 0;
+    success = 0;
+    knock = 0;
+    countDown = 30;
 }
